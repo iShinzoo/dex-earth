df6b2831a2f562aefbe4273d4e7290c3a1eedb35
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2025-07-29 23:55:14 +0530

    feat: ui fix

--- src/components/newHome/PeopleLoveSection.tsx
+++ src/components/newHome/PeopleLoveSection.tsx
@@ -4,7 +4,7 @@ const PeopleLoveSection = () => (
   <section>
     <div className="md:mt-[105px] mt-6 md:mb-[80px] mb-6">
       <h2 className="font-medium md:text-7xl text-[38px] md:leading-[79.2px] md:tracking-[-2.88px] text-center text-[#3DBEA3] mb-[24px]">
-        People ❤️ NCOG
+        People ❤️ DEX
       </h2>
       <p className="font-normal md:text-base text-sm md:leading-[25px] text-center text-[#767676]">
         Mojek is packed with neat features that further assist you in operating
--- src/components/newHome/SecurelyConnectsSection.tsx
+++ src/components/newHome/SecurelyConnectsSection.tsx
@@ -11,7 +11,7 @@ const SecurelyConnectsSection = () => (
       />
       <div className="max-w-[1050px] lg:m-[100px_auto_150px_auto] md:m-[80px_auto_80px_auto] m-[50px_auto_50px_auto]">
         <h2 className="lg:text-[64px] md:text-[42px] text-[22px] font-medium text-[#CFCFCF] mb-4 text-center lg:leading-[76.8px]">
-          NCOG securely connects to your accounts to give a clear picture of your finances and help you lead a healthier
+          DEX securely connects to your accounts to give a clear picture of your finances and help you lead a healthier
           financial life.
         </h2>
         <p className="max-w-[650px] mx-auto font-normal md:text-sm text-xs text-[#767676] text-center">
