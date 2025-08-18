32dd270e000b8eb55aa9efebd7de1e3c0c745299
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2025-08-19 01:42:35 +0530

    fix

--- src/newComponents/Footer.tsx
+++ src/newComponents/Footer.tsx
@@ -6,10 +6,15 @@ const Footer = () => {
       <div className="container mx-auto md:px-6 px-4 pb-[30px] pt-[56px]">
         <h1 className="w-full text-center text-[20vw] leading-none font-bold text-[#2A8576]">DEX</h1>
         <div className="flex justify-between md:flex-row flex-col-reverse md:items-end mt-[90px] md:gap-4 gap-8">
-          <a href="https://ncog.earth/" 
-  target="_blank" rel="noopener noreferrer" className="font-light text-xs leading-[13.2px] text-[#767676] md:text-start text-center">
-            ©2025 Powered by NCOGEARTH
-          </a>
+        <a 
+  href="https://ncog.earth/" 
+  target="_blank" 
+  rel="noopener noreferrer" 
+  className="!underline decoration-black text-black font-light text-xs leading-[13.2px] text-[#767676] md:text-start text-center"
+>
+  ©2025 Powered by Ncog.earth
+</a>
+
           <div className="flex lg:gap-[90px] md:gap-[50px] gap-[25px] font-normal sm:text-[12px] text-[10px] leading-8 text-[#767676]">
             <div className="flex flex-col md:text-center ">
               <a href="#">Integrations</a>
